2e168b55caae8000e3afa297c137d02b14097488
Giulio Dudine <giuliodudine@gmail.com>
2012-10-26 00:54:28 +0200

    setting proper titles

--- csv.php
+++ csv.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Home page</title>
+        <title>CSV manager</title>
         <style>
             *{
                 font-family: arial;
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Home page</title>
+        <title>Login page</title>
     </head>
     <body>
         <?php
